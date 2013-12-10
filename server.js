abd20f2f86c64785b0d42ef09d5004ef0db826f9
Án Bình Trọng <minhtam.seta@gmail.com>
2013-12-10 23:24:16 +0700

    Chat Server by using WebSocket

--- server.js
+++ server.js
@@ -5,9 +5,18 @@ socket.sockets.on('connection', function(socket){
 	listSockets.push(socket);
 	socket.on('message', function(data){
 		console.log(data);
-		socket.send("Server sent package");
+		//socket.send("data");
+		broadcast(socket, data);
 	});
 	socket.on('disconnect', function(){
 		listSockets.splice(listSockets.indexOf(socket), 1);
 	});
 });
+
+function broadcast(socket, message){
+	for(var i = 0; i < listSockets.length; i++){
+		if(listSockets[i] != socket){
+			socket.send(message);
+		}
+	}
+}
