5edb5c24def55c3630add4ffacc18ce7d65fe103
Án Bình Trọng <minhtam.seta@gmail.com>
2013-12-25 16:58:14 +0700

    Notify who's online

--- server.js
+++ server.js
@@ -7,15 +7,53 @@ socket.sockets.on('connection', function(socket){
 	//listSockets[socket.id] = socket;
 	socket.on('message', function(data){
 		console.log(data);
-		broadcast(socket, data);
+		data = JSON.parse(data);
+		if(data.to == "person"){
+			toPerson(socket, data.message);
+		}
+		else{
+			broadcast(socket, data.message);
+		}
 	});
 	socket.on('getClientName', function(data){
 		socket.name = data;
+		notifyWhoOnline(socket);
 	});
 	socket.on('disconnect', function(){
 		listSockets.splice(listSockets.indexOf(socket), 1);
 	});
 });
+
+function notifyWhoOnline(socket){
+	for(var i = 0; i < listSockets.length; i++){
+		listSockets[i].emit('notifyWhoOnline', {'socketList': getListPeople()});
+//		if(listSockets[i].id == socket.id){
+//			//listSockets[i].emit('notifyWhoOnline', serializeMessage({'socketList': getListPeople()}));
+//			listSockets[i].emit('notifyWhoOnline', {'socketList': getListPeople()});
+//		}
+	}
+}
+
+function serializeMessage(message){
+    return JSON.stringify(message);
+} 
+
+function toPerson(socket, message){
+	for(var i = 0; i < listSockets.length; i++){
+		if(listSockets[i].id == socket.id){
+			listSockets[i].send(message);
+			break;
+		}
+	}
+}
+
+function getListPeople(){
+	var people = [];
+	for(var i = 0; i < listSockets.length; i++){
+		people.push({name: listSockets[i].name, socketId: listSockets[i].id});
+	}
+	return people;
+}
 
 function broadcast(socket, message){
 	for(var i = 0; i < listSockets.length; i++){
